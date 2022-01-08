6059ac52bc2ed798175551da1f028310dfaa7e48
langwenda <langwdalan@gmail.com>
2022-01-08 16:47:31 +0800

    update

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/argon
+git clone https://github.com/kenzok8/small.git package/small
+git clone https://github.com/kenzok8/openwrt-packages.git package/kenzo
+git clone https://github.com/fw876/helloworld.git package/helloworld
